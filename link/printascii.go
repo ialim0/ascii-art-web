a99776a19346e66337e4a59dfbff8f35c1d302d4
ialim0 <i.alim0229@gmail.com>
2023-05-22 18:54:58 +0000

    changes

--- link/printascii.go
+++ link/printascii.go
@@ -7,20 +7,21 @@ import (
 // PrintAscii lit une chaîne de caractères en entrée et affiche l'art ASCII correspondant.
 func PrintAscii(input string, tab [][]string) string {
 	count := 0
+
 	var final string
 	// Parcourir chaque ligne de la chaîne de caractères
 	for i, mots := range strings.Split(input, "\\n") {
+		dd := 0
 		linenumber := 0
 		// Si la ligne n'est pas vide, afficher chaque caractère de la ligne sous forme d'art ASCII
 		if len(mots) != 0 {
 			for linenumber < 8 {
 				for _, bit := range []byte(mots) {
-					if bit > 31 && bit < 127 {
+					if bit >= 31 && bit <= 127 {
 						// Afficher l'art ASCII correspondant à chaque caractère
 						final = final + (tab[bit-32][linenumber])
 					} else if bit == 13 {
-						final = final + "\n"
-
+						dd = 1
 					} else {
 						return "Error"
 
@@ -33,6 +34,10 @@ func PrintAscii(input string, tab [][]string) string {
 			// Si la ligne est vide mais qu'il y a eu au moins une ligne non vide avant, ajouter une ligne vide
 		} else if (i != len(strings.Split(input, "\\n"))-1) || count != 0 {
 			final = final + "\n"
+		}
+		if dd == 1 {
+			final = final + "\n"
+
 		}
 	}
 	return final
